e870edeab871a60832b9272903e8a770754e2e7f
Pedro Antunes <pedronmantunes@gmail.com>
2022-05-03 00:08:23 +0100

    verilog testbench working

--- hardware/simulation/icarus/system_tb.sv
+++ hardware/simulation/icarus/system_tb.sv
@@ -21,10 +21,10 @@ module system_tb;
   reg reset = 0;
 
   // DUT inputs
-  wire                 valid;
-  wire [`ADDR_W-1:0] address;
-  wire [`DATA_W-1:0]   wdata;
-  wire [`DATA_W/8-1:0]  strb;
+  reg                 valid;
+  reg [`ADDR_W-1:0] address;
+  reg [`DATA_W-1:0]   wdata;
+  reg [`DATA_W/8-1:0] wstrb;
 
   // DUT outputs
   wire               ready;
@@ -33,11 +33,16 @@ module system_tb;
   wire [`N_CORES-1:0] msip;
 
   integer i = 0;
-  wire [63:0] timer_read;
+  reg [63:0] timer_read;
 
   initial begin
     //assert reset
     #100 reset = 1;
+    valid = 0;
+    address = 0;
+    wdata = 0;
+    wstrb = 0;
+    timer_read = 0;
 
     // deassert rst
     repeat (100) @(posedge clk) #1;
@@ -45,74 +50,79 @@ module system_tb;
 
     //wait an arbitray (10) number of cycles
     repeat (10) @(posedge clk) #1;
-
+    set_inputs(`MTIMECMP_BASE, 20, 15, timer_read[31:0]);
+    set_inputs(`MTIMECMP_BASE+4, 0, 15, timer_read[31:0]);
     while(1) begin
         if(mtip > 0)begin
-            $display("Machine Timer Interrupt is trigere.n");
-            set_inputs(MSIP_BASE, 1, 15);
+            $display("Machine Timer Interrupt is trigere.");
+            set_inputs(`MSIP_BASE, 1, 15, timer_read[31:0]);
         end
         if(msip > 0)begin
-            printf("Machine Software Interrupt is trigered.");
-            set_inputs(MSIP_BASE, 0, 15);
-            timer_read = get_time();
-            $display("Timer count: %ld", timer_read);
-            set_inputs(MTIME_BASE, 0, 15);
-            set_inputs(MTIMECMP_BASE, RTC_PERIOD*100, 15);
+            $display("Machine Software Interrupt is trigered.");
+            set_inputs(`MSIP_BASE, 0, 15, timer_read[31:0]);
+            get_time(timer_read);
+            $display("Timer count: %0d.", timer_read);
+            set_inputs(`MTIME_BASE, 0, 15, timer_read[31:0]);
+            set_inputs(`MTIMECMP_BASE, rtc_per*100, 4'hF, timer_read[31:0]);
         end
-        #(clk_per)
+        @ (posedge clk) #1
         i = i + clk_per;
-        if (i>RTC_PERIOD*100)begin
-          #(clk_per)
+        if (i>rtc_per*100)begin
+          @ (posedge clk) #1
           $display("Testbench finished!");
           $finish;
         end
     end
   end
 
-  myclint clint
-    (
-     //CPU interface
-     .clk     (clk),
-     .rt_clk  (rtc),
-     .reset   (reset),
+  myclint clint (
+    //CPU interface
+    .clk     (clk),
+    .rt_clk  (rtc),
+    .reset   (reset),
 
-     .valid   (valid),
-     .address (address),
-     .wdata   (wdata),
-     .wstrb   (strb),
-     .rdata   (rdata),
-     .ready   (ready),
+    .valid   (valid),
+    .address (address),
+    .wdata   (wdata),
+    .wstrb   (wstrb),
+    .rdata   (rdata),
+    .ready   (ready),
 
-     .mtip    (mtip),
-     .msip    (msip)
-     );
+    .mtip    (mtip),
+    .msip    (msip)
+  );
 
   task wait_responce;
-   output [31:0] data_read;
-   while(ready != 1)
-      #(clk_per)
-   data_read = rdata;
+    output [31:0] data_read;
+    begin
+      while(ready != 1)
+        @ (posedge clk) #1
+      data_read = rdata;
+    end
   endtask
 
   task set_inputs;
-   input [31:0]  set_address;
-   input [31:0]  set_data;
-   input [3:0]   set_strb;
-   output [31:0] data_read;
-   valid = 1;
-   address = set_address;
-   wdata = set_data;
-   wstrb = set_strb;
-   #(clk_per)
-   valid = 0;
-   data_read = wait_responce();
+    input [31:0]  set_address;
+    input [31:0]  set_data;
+    input [3:0]   set_strb;
+    output [31:0] data_read;
+    begin
+      valid = 1;
+      address = set_address;
+      wdata = set_data;
+      wstrb = set_strb;
+      @ (posedge clk) #1 valid = 0;
+      wait_responce(data_read);
+    end
   endtask
 
   task get_time;
-   output [63:0] read_reg;
-
-   read_time[31:0]  = set_inputs(MTIME_BASE, 0, 0);
-   read_time[63:32] = set_inputs(MTIME_BASE+4, 0, 0);
+    output [63:0] read_time;
+    begin
+      set_inputs(`MTIME_BASE, 0, 0, read_time[31:0]);
+      set_inputs(`MTIME_BASE+4, 0, 0, read_time[63:32]);
+    end
   endtask
+
 
 endmodule
