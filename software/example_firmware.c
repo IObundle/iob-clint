abaab8677facacb1949c0f119848ea869decfc11
Pedro Antunes <pedronmantunes@gmail.com>
2022-05-13 16:43:52 +0100

    update example

--- software/example_firmware.c
+++ software/example_firmware.c
@@ -1,5 +1,5 @@
-#include "stdlib.h"
 #include <stdio.h>
+#include <stdint.h>
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
@@ -8,16 +8,20 @@
 
 int main()
 {
-  unsigned long long elapsed;
+    unsigned long long elapsed = 0;
 
-  //init uart
-  uart_init(UART_BASE,FREQ/BAUD);
-  clint_init(CLINT_BASE);
+    //init uart
+    uart_init(UART_BASE,FREQ/BAUD);
+    clint_init(CLINT_BASE);
 
-  uart_puts("\n\n\nHello world!\n\n\n");
-  printf("Value of Pi = %f\n\n", 3.1415);
+    // clint_set_timer(0);
 
-  elapsed = clint_get_timer();
-  printf("\nCLINT timer value: %d\n", (unsigned int) elapsed);
-  uart_finish();
+    uart_puts("\n\n\nHello world!\n\n\n");
+    printf("Value of Pi = %f\n\n", 3.1415);
+
+    elapsed = clint_get_timer();
+    printf("\nCLINT timer value: %llu\n", elapsed);
+
+    printf("CLINT elapsed time: %f\n", elapsed/32768.0);
+    uart_finish();
 }
