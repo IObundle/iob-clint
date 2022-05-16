5788d4f4a6b363e38423db8904affe84f15cec3f
Pedro Antunes <pedronmantunes@gmail.com>
2022-05-16 17:47:14 +0100

    included csr, interupts and clint timer headers. In accordance to RISC-V

---
+++ software/include/timer.h
@@ -0,0 +1,39 @@
+/*
+   Simple machine mode timer driver for RISC-V standard timer.
+   SPDX-License-Identifier: Unlicense
+
+   (https://five-embeddev.com/)
+
+*/
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <stdint.h>
+
+//#define CLINT_BASE (1<<31) |(CLINT<<(31-N_SLAVES_W))
+#ifndef CLINT_BASE
+  #define CLINT_BASE 0xC0000000
+#endif
+#define RISCV_MTIMECMP_ADDR (CLINT_BASE + 0x4000)
+#define RISCV_MTIME_ADDR    (CLINT_BASE + 0xBFF8)
+
+#define MTIME_FREQ_HZ 32768
+
+#define MTIMER_SECONDS_TO_CLOCKS(SEC)           \
+    ((uint64_t)(((SEC)*(MTIME_FREQ_HZ))))
+
+/** Set the raw time compare point in system timer clocks.
+ * @param clock_offset Time relative to current mtime when
+ * @note The time range of the 64 bit timer is large enough not to consider a wrap around of mtime.
+ * An interrupt will be generated at mtime + clock_offset.
+ * See http://five-embeddev.com/riscv-isa-manual/latest/machine.html#machine-timer-registers-mtime-and-mtimecmp
+ */
+void mtimer_set_raw_time_cmp(uint64_t clock_offset);
+
+/** Read the raw time of the system timer in system timer clocks
+ */
+uint64_t mtimer_get_raw_time(void);
+
+
+#endif // #ifdef TIMER_H
--- software/software.mk
+++ software/software.mk
@@ -1,10 +1,13 @@
 include $(CLINT_DIR)/config.mk
 
+
+CLINT_SW_INC_DIR = $(CLINT_SW_DIR)/include
+
 #include
-INCLUDE+=-I$(CLINT_SW_DIR)
+INCLUDE+=-I$(CLINT_SW_DIR) -I$(CLINT_SW_INC_DIR)
 
 #headers
-HDR+=$(CLINT_SW_DIR)/*.h
+HDR+=$(CLINT_SW_DIR)/*.h $(CLINT_SW_INC_DIR)/*.h
 
 #sources
-SRC+=$(CLINT_SW_DIR)/myclint.c
+SRC+=$(CLINT_SW_DIR)/myclint.c $(CLINT_SW_INC_DIR)/*.c
